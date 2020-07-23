38f83a3fcd2f64491886908c5c02358a29de7c6a
ChenZebedee <dishaochen@yahoo.com>
2020-07-23 13:56:34 +0800

    fix

--- bigdata/Kafka/KAFKA_ERROR.md
+++ bigdata/Kafka/KAFKA_ERROR.md
@@ -1,12 +1,7 @@
 # shell 工具执行时端口冲突的解决方法
 将 `kafka-run-class.sh` 文件中的内容修改
 ```
-# JMX port to use 
-if [ $JMX_PORT ]; then 
-KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Dcom.sun.management.jmxremote.port=$JMX_PORT" 
-fi 
 
-====>
 
 # JMX port to use 
 if [[ $ISKAFKASERVER = "true" ]]; then 
--- bigdata/Kafka/kafka_test.sh
+++ bigdata/Kafka/kafka_test.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-<<<<<<< HEAD
-
-list=`seq $1`
-parNum=$2
-numRecords=$3
-recordSize=$4
-runWhat=$5
-=======
 allTopicNum=$1
 list=`seq ${allTopicNum}`
 parNum=$2
@@ -16,48 +8,27 @@ recordSize=$4
 morePartitionTopic=$5
 runWhat=$6
 onePartitionTopic=$[${allTopicNum}-${morePartitionTopic}]
->>>>>>> 28385cb26b0f52ca03a1ea8e8a59a7c3a486c4f1
 
 
 create(){
 for i in ${list}
 do
-<<<<<<< HEAD
-kafka-topics.sh --create --zookeeper data1:2181,data2:2181,data3:2181 --replication-factor 2 --partitions ${parNum} --topic "test-${i}" 2>&1 > log/create/test-${i}
-=======
 mkdir -p log/create/${parNum}/${allTopicNum};
 kafka-topics.sh --create --zookeeper data1:2181,data2:2181,data3:2181 --replication-factor 1 --partitions ${parNum} --topic "test-${parNum}-${i}" 2>&1 > log/create/${parNum}/${allTopicNum}/test-${parNum}-${i}
->>>>>>> 28385cb26b0f52ca03a1ea8e8a59a7c3a486c4f1
 done
 }
 
 producer(){
 for i in ${list}
 do
-<<<<<<< HEAD
-mkdir -p log/producer/${parNum}/${numRecoure}/${recordSize};
-kafka-producer-perf-test.sh --topic "test-${i}" --num-records ${numRecords} --record-size ${recordSize} --throughput -1 --producer-props bootstrap.servers=data1:9092,data2:9092,data3:9092 acks=-1 2>&1 > log/producer/${parNum}/${numRecoure}/${recordSize}/test-${i} &
-=======
 mkdir -p log/producer/${parNum}/${allTopicNum};
 kafka-producer-perf-test.sh --topic "test-${parNum}-${i}" --numRecords ${numRecords} --recordSize ${recordSize} --throughput -1 --producer-props bootstrap.servers=data1:9092 acks=-1 2>&1 > log/producer/${parNum}/${allTopicNum}/test-${parNum}-${i}
->>>>>>> 28385cb26b0f52ca03a1ea8e8a59a7c3a486c4f1
 done
 }
 
 consumer(){
 for i in ${list}
 do
-<<<<<<< HEAD
-mkdir -p log/consumer/${numRecords}
-kafka-consumer-perf-test.sh --broker-list data1:9092,data2:9092,data3:9092  --topic test-${i} --messages ${numRecords} 2>&1 > log/consumer/${numRecords}/test-${i} &
-done
-}
-
-delete(){
-for i in ${list}
-do
-kafka-topics.sh -delete -zookeeper data1:2181,data2:2181,data3:2181 -topic test-${i}
-=======
 mkdir -p log/consumer/${parNum}/${allTopicNum}
 kafka-consumer-perf-test.sh --broker-list data1:9092  --topic test-${parNum}-${i} --messages ${numRecords}  --num-fetch-threads ${parNum} --threads ${parNum} 2>&1 > log/consumer/${parNum}/${allTopicNum}/test-${parNum}-${i} &
 done
@@ -86,7 +57,6 @@ delete(){
 for i in ${list}
 do
 kafka-topics.sh --delete --zookeeper data1:2181,data2:2181,data3:2181 --topic test-${parNum}-${i}
->>>>>>> 28385cb26b0f52ca03a1ea8e8a59a7c3a486c4f1
 done
 }
 
@@ -100,21 +70,11 @@ producer
 elif [  ${runWhat} == "3" ]
 then
 consumer
-<<<<<<< HEAD
-elif [ ${runWhat} == "4" ]
-=======
 elif [  ${runWhat} == "4" ]
->>>>>>> 28385cb26b0f52ca03a1ea8e8a59a7c3a486c4f1
 then
 delete
 elif [  ${runWhat} == "5" ]
 then
-<<<<<<< HEAD
-create
-producer
-consumer
-fi
-=======
 delete
 create
 producer
@@ -124,4 +84,3 @@ then
 business_consumer
 fi
 
->>>>>>> 28385cb26b0f52ca03a1ea8e8a59a7c3a486c4f1
